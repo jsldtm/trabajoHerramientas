440b184bcc7b304dc75b17fa03011192ca06eb10
Joaquín Saldarriaga <jsaldatam@outlook.com>
2024-03-21 15:44:15 -0600

    Modificacion 1 a juego Paint.py

--- paint.py
+++ paint.py
@@ -1,24 +1,26 @@
+
 """Paint, for drawing shapes.
 
 Exercises
 
-1. Add a color.
-2. Complete circle.
-3. Complete rectangle.
-4. Complete triangle.
-5. Add width parameter.
+1. Added color (already implemented) # ya está
+2. Complete circle (implemented here) # ya está
+3. Complete rectangle. (not modified)
+4. Complete triangle. (not modified)
+5. Added width parameter. (not modified)
 """
 
 from turtle import *
+from random import randrange, choice
 
 from freegames import vector
-
 
 def line(start, end):
     """Draw line from start to end."""
     up()
     goto(start.x, start.y)
     down()
+    pencolor(get_random_color())  # Se define un 'color' aleatorio para cada segmento de línea
     goto(end.x, end.y)
 
 
@@ -36,42 +38,54 @@ def square(start, end):
     end_fill()
 
 
+## Función círculo:
 def circle(start, end):
-    """Draw circle from start to end."""
-    pass  # TODO
+    """Dibuje un círculo de inicio a fin:"""
+    up()
+    goto(start.x, start.y)
+    down()
+    pencolor(color)  # Set color before drawing
+    radius = (end.x - start.x) // 2  # Calculate circle radius based on start and end points
+    circle(start.x, start.y)
 
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
-    pass  # TODO
+    pass  # TODO (not modified)
 
 
 def triangle(start, end):
     """Draw triangle from start to end."""
-    pass  # TODO
+    pass  # TODO (not modified)
 
 
 def tap(x, y):
-    """Store starting point or draw shape."""
+    """Start or continue drawing shape."""
     start = state['start']
 
     if start is None:
         state['start'] = vector(x, y)
     else:
-        shape = state['shape']
         end = vector(x, y)
+        shape = state['shape']
         shape(start, end)
-        state['start'] = None
+        state['start'] = end  # Update start point for continuous drawing
+
+
+def get_random_color():
+    """Generando un color aleatorio"""
+    colors = ['red', 'green', 'blue', 'yellow', 'purple', 'orange']
+    return choice(colors)
 
 
 def store(key, value):
-    """Store value in state at key."""
+    """Store value in state at key"""
     state[key] = value
 
 
 state = {'start': None, 'shape': line}
 setup(420, 420, 370, 0)
-onscreenclick(tap)
+onscreenclick(tap)  # Call tap on click (hold)
 listen()
 onkey(undo, 'u')
 onkey(lambda: color('black'), 'K')
